40f03227260d2f319009f2a49e97db629c60e98b
diouf <geoffreyfleur@gmail.com>
2023-12-05 16:25:54 +0100

    :ukraine: Créations des différents graphiques avec nivo

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,18 @@ import {CssBaseline, ThemeProvider} from "@mui/material";
 import {Routes, Route, Router} from "react-router-dom";
 import Topbar from '@/scenes/global/Topbar';
 import Dashboard from "@/scenes/dashboard";
-import ProSidebar from "@/scenes/global/Sidebar";
 import Sidebar from "@/scenes/global/Sidebar";
 import Team from "@/scenes/team";
 import Invoices from "@/scenes/invoices";
 import Contacts from "@/scenes/contacts";
 import Form from "@/scenes/form";
 import Calendar from "@/scenes/calendar";
-// import Bar from "@/scenes/bar";
-// import Line from "@/scenes/line";
-// import Pie from "@/scenes/pie";
-// import FAQ from "@/scenes/faq";
+import FAQ from "@/scenes/faq";
+import Bar from "@/scenes/bar";
+import Pie from "@/scenes/pie";
+import Line from "@/scenes/line";
+// import ProSidebar from "@/scenes/global/Sidebar";
 // import Geography from "@/scenes/geography";
-
 
 function App() {
 
@@ -36,10 +35,10 @@ function App() {
                             <Route path='/invoices' element={<Invoices />} />
                             <Route path='/form' element={<Form />} />
                             <Route path='/calendar' element={<Calendar />} />
-                            {/*<Route path='/bar' element={<Bar />} />*/ }
-                            {/*<Route path='/pie' element={<Pie />} />*/ }
-                            {/*<Route path='/line' element={<Line />} />*/ }
-                            {/*<Route path='/faq' element={<FAQ />} />*/ }
+                            <Route path='/bar' element={<Bar />} />
+                            <Route path='/pie' element={<Pie />} />
+                            <Route path='/line' element={<Line />} />
+                            <Route path='/faq' element={<FAQ />} />
                             {/*<Route path='/geography' element={<Geography />} />*/ }
                         </Routes>
                     </main>

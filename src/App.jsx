f71def9883ba02f8e5844f2cf711074cbd67dbf1
diouf <geoffreyfleur@gmail.com>
2023-12-05 13:57:15 +0100

    :ukraine: Créations du calendrier interactif PB à résoudre concernant le prompte

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,13 @@ import Sidebar from "@/scenes/global/Sidebar";
 import Team from "@/scenes/team";
 import Invoices from "@/scenes/invoices";
 import Contacts from "@/scenes/contacts";
-// import Bar from "@/scenes/bar";
 import Form from "@/scenes/form";
+import Calendar from "@/scenes/calendar";
+// import Bar from "@/scenes/bar";
 // import Line from "@/scenes/line";
 // import Pie from "@/scenes/pie";
 // import FAQ from "@/scenes/faq";
 // import Geography from "@/scenes/geography";
-// import Calendar from "@/scenes/calendar";
 
 
 function App() {
@@ -35,12 +35,12 @@ function App() {
                             <Route path='/contacts' element={<Contacts />} />
                             <Route path='/invoices' element={<Invoices />} />
                             <Route path='/form' element={<Form />} />
+                            <Route path='/calendar' element={<Calendar />} />
                             {/*<Route path='/bar' element={<Bar />} />*/ }
                             {/*<Route path='/pie' element={<Pie />} />*/ }
                             {/*<Route path='/line' element={<Line />} />*/ }
                             {/*<Route path='/faq' element={<FAQ />} />*/ }
                             {/*<Route path='/geography' element={<Geography />} />*/ }
-                            {/*<Route path='/calendar' element={<Calendar />} />*/ }
                         </Routes>
                     </main>
                 </div>
